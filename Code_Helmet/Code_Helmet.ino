5fc6d4bc337b85e6792db23ab2726d334f66f511
KKhang <127233909+SevKan10@users.noreply.github.com>
2023-04-10 15:30:15 +0700

    add math radians

--- Code_Helmet/Code_Helmet.ino
+++ Code_Helmet/Code_Helmet.ino
@@ -9,7 +9,16 @@ TinyGPSPlus gps;
 SoftwareSerial SoftSerial(S_RX, S_TX);
 boolean yellowled = 13;
 boolean redled = 12;
+boolean greenled = 11;
+float initialLatitude = 10.029966;
+float initialLongitude = 105.686610;
+float latitude, longitude;
+
+const float maxDistance = 30; // Tính theo hệ mét
 // Lấy đèn để mô phỏng cho mp3
+
+void getGps(float& latitude, float& longitude); // Make Virtual Fence
+
 void setup()
  
  {
@@ -20,8 +29,10 @@ void setup()
    mpu6050.calcGyroOffsets(true);
    pinMode(yellowled,OUTPUT);
    pinMode(redled,OUTPUT);
+   pinMode(greenled,OUTPUT);
    yellowled = 0;
    redled = 0;
+   greenled = 0;
  }
 
 void loop()
@@ -60,6 +71,74 @@ void loop()
   }
     // Nếu xe chạy trên 40km/h thì đèn vàng sẽ sáng
   }
+  // XỬ LÝ GPS HÀNG RÀO ẢO
+  getGps(latitude, longitude);
+  float distance = getDistance(latitude, longitude, initialLatitude, initialLongitude);
+  Serial.print("Latitude= "); Serial.println(latitude, 6);
+  Serial.print("Lngitude= "); Serial.println(longitude, 6);
+  Serial.print("initialLatitude= "); Serial.println(initialLatitude, 6);
+  Serial.print("initialLngitude= "); Serial.println(initialLongitude, 6);
+  Serial.print("current Distance= "); Serial.println(distance);
+  if (distance > maxDistance)
+  {
+          digitalWrite(greenled,1);
+  }
+  else 
+  {
+          digitalWrite(greenled,0);
+  }
+  float getDistance(float flat1, float flon1, float flat2, float flon2) {
+
+  // Variables
+  float dist_calc=0;
+  float dist_calc2=0;
+  float diflat=0;
+  float diflon=0;
+
+  // Calculations
+  diflat  = radians(flat2-flat1);
+  flat1 = radians(flat1);
+  flat2 = radians(flat2);
+  diflon = radians((flon2)-(flon1));
+
+  dist_calc = (sin(diflat/2.0)*sin(diflat/2.0));
+  dist_calc2 = cos(flat1);
+  dist_calc2*=cos(flat2);
+  dist_calc2*=sin(diflon/2.0);
+  dist_calc2*=sin(diflon/2.0);
+  dist_calc +=dist_calc2;
+
+  dist_calc=(2*atan2(sqrt(dist_calc),sqrt(1.0-dist_calc)));
+  
+  dist_calc*=6371000.0; //Converting to meters
+
+  return dist_calc;
+}
+void getGps(float& latitude, float& longitude)
+{
+  // Can take up to 60 seconds
+  boolean newData = false;
+  for (unsigned long start = millis(); millis() - start < 2000;){
+    while (gps.available()){
+      if (gps.encode(neogps.read())){
+        newData = true;
+        break;
+      }
+    }
+  }
+  
+  if (newData) //If newData is true
+  {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+    newData = false;
+  }
+  else {
+    Serial.println("No GPS data is available");
+    latitude = 0;
+    longitude = 0;
+  }
+}
 //*************************************************
     // XỬ LÝ DỮ LIỆU MPU6050
      mpu6050.update();
